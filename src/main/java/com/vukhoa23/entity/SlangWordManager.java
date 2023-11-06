5d898d1b4ddab74b6d89dbc4e7825383f931677d
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-06 13:26:02 +0700

    Created SlangWordManager entity
    Use TreeMap<String, String> to store Slang Words
    
    Use HashMap<String, Date> to store history

---
+++ src/main/java/com/vukhoa23/entity/SlangWordManager.java
@@ -0,0 +1,12 @@
+package com.vukhoa23.entity;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class SlangWordManager {
+    TreeMap<String, String> slangWords = new TreeMap<>();
+    HashMap<String, Date> history = new HashMap<>();
+
+
+}
