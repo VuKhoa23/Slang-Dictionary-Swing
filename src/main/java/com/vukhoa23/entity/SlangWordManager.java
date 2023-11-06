abf72e8087251bc0a0add45950929df87e799652
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-06 13:28:29 +0700

    Finish following functions:
    Add to SlangWords/History
    
    Delete from SlangWords

--- src/main/java/com/vukhoa23/entity/SlangWordManager.java
+++ src/main/java/com/vukhoa23/entity/SlangWordManager.java
@@ -8,5 +8,15 @@ public class SlangWordManager {
     TreeMap<String, String> slangWords = new TreeMap<>();
     HashMap<String, Date> history = new HashMap<>();
 
+    public void addToHistory(String key, String value) {
+        history.put(key, new Date());
+    }
 
+    public void addToWords(String key, String value) {
+        slangWords.put(key, value);
+    }
+
+    public void deleteAWord(String key){
+        slangWords.remove(key);
+    }
 }
