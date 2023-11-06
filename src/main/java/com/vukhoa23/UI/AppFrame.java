bd9735eb2349444525b3f66bae83ce6db71422fc
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-06 13:40:44 +0700

    Added AppFrame class extends JFrame. This will be the frame for the program

--- src/main/java/com/vukhoa23/UI/AppFrame.java
+++ src/main/java/com/vukhoa23/UI/AppFrame.java
@@ -1,4 +1,26 @@
 package com.vukhoa23.UI;
 
-public class AppFrame {
+import com.vukhoa23.entity.SlangWordManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileNotFoundException;
+
+public class AppFrame extends JFrame {
+    private final SlangWordManager manager = new SlangWordManager();
+
+
+    public AppFrame() throws FileNotFoundException {
+        super("Slang Words");
+
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        manager.readSlangWordsFromFile("slang.txt");
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
+        this.pack();
+    }
+
 }
